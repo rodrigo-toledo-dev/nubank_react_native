6412429dd757337b4d4926ced415021a1066d08b
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2019-06-05 21:19:19 -0300

    menu with styles but hidden and without effects

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '~/components/Header';
+import Menu from '~/components/Menu';
 import Tabs from '~/components/Tabs';
 
 import { Container, Content, Card, CardHeader, CardContent, Title, Description, CardFooter, Annotation } from './styles';
@@ -12,6 +13,8 @@ export default function Main() {
       <Header />
 
       <Content>
+        <Menu />
+
         <Card>
           <CardHeader>
             <Icon name="attach-money" size={28} color="#666" />
